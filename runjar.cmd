9185ecbf9608a972fde1498efdf5d9acf52e63fe
Nouvan Jónsson <nouvanjonsson@gmail.com>
2024-02-06 11:33:18 +0000

    Initial commit

---
+++ runjar.cmd
@@ -0,0 +1,1 @@
+java -cp target/ass4-1.0-SNAPSHOT.jar:target/dependency/commons-numbers-primes-1.1.jar is.hi.mns2.hbv202g.App
---
+++ src/main/java/is/hi/mns2/hbv202g/App.java
@@ -0,0 +1,15 @@
+package is.hi.mns2.hbv202g;
+
+import static org.apache.commons.numbers.primes.Primes.nextPrime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(nextPrime(123456789));
+    }
+}
